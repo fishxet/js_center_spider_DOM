62655de1a86e6f2649414ca4693725d723f76254
Leonid Markevych <ggfhappinesss@gmail.com>
2025-08-28 22:22:26 +0300

    make build

---
+++ index.cc4440e9.js
@@ -0,0 +1,2 @@
+var e=document.querySelector(".wall"),t=document.querySelector(".spider");e.style.position="relative",t.style.position="absolute",t.style.top=(e.clientHeight-t.offsetHeight)/2+"px",t.style.left=(e.clientWidth-t.offsetWidth)/2+"px";
+//# sourceMappingURL=index.cc4440e9.js.map
